fe83f0be6b6635e084711c524664c1d9f4fa2850
ece99 <eceserver.ayvaz@gmail.com>
2022-09-18 10:50:51 +0300

    initial commit

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Ece Ayvaz on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
